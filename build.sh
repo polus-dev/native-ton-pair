57d1295818d6a5863be11ea9a97f13940aaeff6f
cryshado <mirmiclub@gmail.com>
2022-05-07 16:33:13 +0300

    fix build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ func -SPA -o "auto/main.func.code.fif" \
     "func/utils/event.func" \
     "func/utils/op-codes.func" "func/utils/exit-codes.func" \
     "func/utils/storage.func" "func/utils/math.func" "func/utils/msg-utils.func" \
-    "func/utils/admin-methods.func" "func/utils/user-methods.func" \
-    "func/utils/handles.func" "func/utils/get-methods.func" \
+    "func/utils/admin-methods.func" "func/utils/get-methods.func" \
+    "func/utils/user-methods.func" "func/utils/handles.func" \
     "func/main.func"
